1963ac390a0687f3784ab045044294bee6fb158f
German <calletutor@gmail.com>
2025-12-21 16:23:08 +0300

    Добавил implementation Koin, создал DI Koin SearchModule

--- app/src/main/java/ru/practicum/android/diploma/search/di/SearchModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/di/SearchModule.kt
@@ -1,2 +1,21 @@
 package ru.practicum.android.diploma.search.di
 
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+//будет завершено после окончания настройки Network
+val searchModule = module {
+    // Context для NetworkConnectionChecker
+    single { androidContext() }
+
+//    single { NetworkConnectionChecker(get()) }
+
+//    single { provideRetrofit(get()) }
+//    single { provideVacancyApi(get()) }
+
+//    single<NetworkClient> { NetworkClientImpl(get(), get()) }
+
+//    single<VacancyRepository> { VacancyRepositoryImpl(get()) }
+
+//    single<VacancyInteractor> { VacancyInteractorImpl(get()) }
+}
