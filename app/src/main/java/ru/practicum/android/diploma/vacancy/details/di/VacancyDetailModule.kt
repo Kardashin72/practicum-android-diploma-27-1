85357d39541ec9a9f3dc5e3ba90b825538964838
German <calletutor@gmail.com>
2025-12-22 21:18:29 +0300

    корректировка detekt

--- app/src/main/java/ru/practicum/android/diploma/vacancy/details/di/VacancyDetailModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/details/di/VacancyDetailModule.kt
@@ -4,6 +4,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.vacancy.details.presentation.viewmodel.VacancyDetailsViewModel
 
+// пустой коммент
 val VacancyDetailViewModule = module {
     viewModel { VacancyDetailsViewModel(get()) }
 }
