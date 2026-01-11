ea50d92ff247fb9ce4d697a1adaf5685c0637f2d
German <calletutor@gmail.com>
2026-01-11 12:52:53 +0300

    скрывю клавиатуру после поиска + корректировка "Таких вакансий нет" как по фигме

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/ui/SearchScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/ui/SearchScreen.kt
@@ -20,12 +20,14 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -60,6 +62,14 @@ fun SearchScreen(
     val hasActiveFilters by viewModel.hasActiveFilters.collectAsState()
 
     val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    // Скрываю клавиатуру при получении результатов поиска
+    LaunchedEffect(searchState) {
+        if (searchState is SearchState.Content) {
+            keyboardController?.hide()
+        }
+    }
 
     if (paginationErrorMessage != null) {
         val toastText = when (paginationErrorMessage) {
